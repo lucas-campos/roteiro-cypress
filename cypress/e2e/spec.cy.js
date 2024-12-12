d261f18cf87ea26b9a83f10a9dcec2b278bf39f0
lucas campos <lucascampos.mg@gmail.com>
2024-12-11 22:45:45 -0300

    teste e2e

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -68,4 +68,77 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+
+  // Novos testes
+  it('Insere duas tarefas', () => {
+    cy.visit(''); 
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP1 de Engenharia de Software 2024-Lucas{enter}');
+    
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de Engenharia de Software 2024-Lucas{enter}');
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 2) 
+      .first()
+      .should('have.text', 'TP1 de Engenharia de Software 2024-Lucas')
+      .next()
+      .should('have.text', 'TP2 de Engenharia de Software 2024-Lucas');
+  });
+
+  it('Insere e deleta duas tarefas', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP1 de Engenharia de Software para deletar{enter}');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de Engenharia de Software para deletar{enter}');
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 2);
+
+    cy.get('[data-cy=todos-list] > li [data-cy=remove-todo-btn]')
+      .first()
+      .invoke('show')
+      .click();
+
+    cy.get('[data-cy=todos-list] > li [data-cy=remove-todo-btn]')
+      .first()
+      .invoke('show')
+      .click();
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 0);
+  });
+
+  it('Insere três tarefas e deleta apeans a primeira', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP1 de Engenharia de Software para deletar{enter}');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de Engenharia de Software para deletar{enter}');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP3 de Engenharia de Software para deletar{enter}');
+    
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 3);
+
+    cy.get('[data-cy=todos-list] > li [data-cy=remove-todo-btn]')
+      .first()
+      .invoke('show')
+      .click();
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 2);
+  });
 });
